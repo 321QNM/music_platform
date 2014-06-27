11d0fc498602f49c07e5699cd40140ec8121316e
yxjxx <yangjing201111335@gmail.com>
2014-06-27 20:52:35 +0800

    针对注册页面的三次post分别写一个if分支,分开处理

--- handlers/signup.py
+++ handlers/signup.py
@@ -12,16 +12,20 @@ class SignupHandler(BaseHandler):
         self.render('signup.html')
 
     def post(self):
-        email = self.get_argument("email", "default_email")
-        username = self.get_argument("username", "default_username")
-        password = self.get_argument("password1", "default_password")
-        invite_code = self.get_argument("invitecode", "default_invitecode")
+        email = self.get_argument("email", "default")
+        username = self.get_argument("username", "default")
+        password = self.get_argument("password", "default")
+        invite_code = self.get_argument("invitecode", "default")
 
-        if is_username_existed(username):
-            self.write("existed")
-        if is_valid_invite_code(invite_code):
-            self.write("is_valid_invite_code")
-        if not is_username_existed(username) and is_valid_invite_code(invite_code):
-            insert_new_user(username, password)
-            self.set_secure_cookie('username', username)
-            self.redirect(self.get_argument('next', '/'))
+        if username != "default" and email == "default" and password == "default" and invite_code == "default":
+            if is_username_existed(username):
+                self.write("existed")
+        if invite_code != "default" and email == "default" and password == "default" and username == "default":
+            if not is_valid_invite_code(invite_code):
+                self.write("is_not_valid_invite_code")
+        # self.write(email+username+password+invite_code)
+        if username != "default" and email != "default" and password != "default" and invite_code != "default":
+            if not is_username_existed(username) and is_valid_invite_code(invite_code):
+                insert_new_user(username, password)
+                self.set_secure_cookie('username', username)
+                self.redirect('/')
