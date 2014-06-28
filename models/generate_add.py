ee9f6747a26672178c6f3fa0ec6a98c550c4b820
yxjxx <yangjing201111335@gmail.com>
2014-06-28 11:08:49 +0800

    增加自动生成待填充的music_origin_info文件的脚本

---
+++ models/generate_add.py
@@ -0,0 +1,56 @@
+f = open('music_origin_info.py','a')
+
+f.write("#codeing=utf-8\n")
+f.write("music_list = [\n")
+for i in range(1,101):
+    f.write("    dict(\n")
+    f.write("        music_name = '',\n")
+    f.write("        music_publish_date = '',\n")
+    f.write("        music_url = path +'',\n")
+    f.write("        music_artist = '',\n")
+    f.write("        music_zone = '',\n")
+    f.write("        music_style = '',\n")
+    f.write("        music_mood = ''\n")
+    f.write("    ),\n")
+f.write("]")
+
+
+    # dict(
+    #     music_name = 'a',
+    #     music_publish_date = '2014/5/2',
+    #     music_url = path +'We are the world',
+    #     music_artist = 'Mike',
+    #     music_zone = 'taiwan',
+    #     music_style = 'pop',
+    #     music_mood = 'happy'
+    # ),
+
+
+# #codeing=utf-8
+# from database import *
+# path = '/home/yxj'
+# music_list = [
+#     dict(
+#         music_name = '',
+#         music_publish_date = '',
+#         music_url = path +'',
+#         music_artist = '',
+#         music_zone = '',
+#         music_style = '',
+#         music_mood = ''
+#     ),
+#     dict(
+#         music_name = '',
+#         music_publish_date = '',
+#         music_url = path +'',
+#         music_artist = '',
+#         music_zone = '',
+#         music_style = '',
+#         music_mood = ''
+#     ),
+# ]
+
+# for music in music_list:
+#     insert_music_to_database(music)
+#     # print(music)
+
