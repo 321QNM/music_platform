f0703064b7cbc61b2a849934e854a39e64af87c7
a32553411 <32553411@qq.com>
2014-06-28 10:37:48 +0800

    开始填写音乐详细信息

--- models/music_origin_info.py
+++ models/music_origin_info.py
@@ -1,13 +1,13 @@
 #codeing=utf-8
 music_list = [
     dict(
-        music_name = '',
-        music_publish_date = '',
-        music_url = path +'',
-        music_artist = '',
-        music_zone = '',
-        music_style = '',
-        music_mood = ''
+        music_name = 'Stay Beautiful',
+        music_publish_date = '2014/05/03',
+        music_url = path +'Stay Beautiful',
+        music_artist = 'Taylor Swift',
+        music_zone = 'USA',
+        music_style = 'folk',
+        music_mood = 'happy'
     ),
     dict(
         music_name = '',
