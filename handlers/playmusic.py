adb3640499a3dfcf49f8d81e54481444265cbd41
yxjxx <yangjing201111335@gmail.com>
2014-06-28 15:48:59 +0800

    修改playmusic.py传递参数的方式,更加pythonic

--- handlers/playmusic.py
+++ handlers/playmusic.py
@@ -6,5 +6,14 @@ from handlers.base import BaseHandler
 class PlayMusicHandler(BaseHandler):
     @tornado.web.authenticated
     def get(self):
-        self.render('playmusic.html', user=self.current_user, music_source="../static/music/1.mp3")
+
+        # items = ["Item 1", "Item 2", "Item 3"]
+        music_source="../static/music/1.mp3"
+        params = {
+            "user" : self.current_user,
+            "music_source" : music_source,
+            # "items": items,
+        }
+        self.render(u"playmusic.html", **params)
+        # self.render('playmusic.html', user=self.current_user, music_source="../static/music/1.mp3")
 
