78626d9b8779758ac35dc1fda0daddea98d08b24
yxjxx <yangjing201111335@gmail.com>
2014-07-01 09:56:42 +0800

    成功显示背景图片

--- handlers/playmusic.py
+++ handlers/playmusic.py
@@ -9,8 +9,6 @@ from models.database import *
 class PlayMusicHandler(BaseHandler):
     @tornado.web.authenticated
     def get(self):
-        # music_url_list = personal_recommend()
-        # music_url = music_url_list[ random.randint(0, len(music_url_list)-1 )]["music_url"]
         # # items = ["Item 1", "Item 2", "Item 3"]
         # params = {
         #     "user" : self.current_user,
@@ -23,9 +21,16 @@ class PlayMusicHandler(BaseHandler):
 
     @tornado.web.authenticated
     def post(self):
+        music_list = personal_recommend()
+        current_music = music_list[ random.randint(0, len(music_list)-1 )]
+        current_music_url = current_music["music_url"]
+        current_music_name = current_music["music_name"]
+        current_music_artist = current_music["music_artist"]
+        current_music_picture_url = current_music["music_picture_url"]
         obj = {
-            'music_url': "../static/music/1.mp3",
-            'music_name': "God is a girl",
-            'music_artist': "Taylor swift"
+            'music_url': current_music_url,
+            'music_name': current_music_name,
+            'music_artist': current_music_artist,
+            'music_picture_url': current_music_picture_url,
         }
         self.write(json_encode(obj))
