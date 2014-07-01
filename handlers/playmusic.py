dab8e0b58a83d607dfc4515c9d5a173d1732fc65
yxjxx <yangjing201111335@gmail.com>
2014-07-01 10:37:32 +0800

    通过字符串的比对区分不同的post

--- handlers/playmusic.py
+++ handlers/playmusic.py
@@ -6,6 +6,21 @@ from tornado.escape import json_encode
 import random
 from models.database import *
 
+def generate_new_music():
+    music_list = personal_recommend()
+    current_music = music_list[ random.randint(0, len(music_list)-1 )]
+    current_music_url = current_music["music_url"]
+    current_music_name = current_music["music_name"]
+    current_music_artist = current_music["music_artist"]
+    current_music_picture_url = current_music["music_picture_url"]
+    send_music_json = {
+        'music_url': current_music_url,
+        'music_name': current_music_name,
+        'music_artist': current_music_artist,
+        'music_picture_url': current_music_picture_url,
+    }
+    return send_music_json
+
 class PlayMusicHandler(BaseHandler):
     @tornado.web.authenticated
     def get(self):
@@ -21,16 +36,23 @@ class PlayMusicHandler(BaseHandler):
 
     @tornado.web.authenticated
     def post(self):
-        music_list = personal_recommend()
-        current_music = music_list[ random.randint(0, len(music_list)-1 )]
-        current_music_url = current_music["music_url"]
-        current_music_name = current_music["music_name"]
-        current_music_artist = current_music["music_artist"]
-        current_music_picture_url = current_music["music_picture_url"]
-        obj = {
-            'music_url': current_music_url,
-            'music_name': current_music_name,
-            'music_artist': current_music_artist,
-            'music_picture_url': current_music_picture_url,
-        }
-        self.write(json_encode(obj))
+        action = self.get_argument("action", "default")
+
+        if action == "refresh":
+            send_music_json = generate_new_music()
+            self.write(json_encode(send_music_json))
+
+        if action == "like":
+            pass
+
+        if action == "hate":
+            send_music_json = generate_new_music()
+            self.write(json_encode(send_music_json))
+
+        if action == "end":
+            send_music_json = generate_new_music()
+            self.write(json_encode(send_music_json))
+
+        if action == "next":
+            send_music_json = generate_new_music()
+            self.write(json_encode(send_music_json))
