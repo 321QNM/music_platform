d4f8f8e69f872e207fdd5b95b9fcda3c55aab4bb
yxjxx <yangjing201111335@gmail.com>
2014-07-02 17:06:31 +0800

    AdminIndexHandler 先前端发送多首歌曲列表

--- handlers/adminindex.py
+++ handlers/adminindex.py
@@ -2,6 +2,28 @@
 
 import tornado.web
 from handlers.adminbase import AdminBaseHandler
+from models.database import *
+from tornado.escape import json_encode
+
+
+def generate_musics_table():
+    music_list = personal_recommend()
+    to_send_music_info_list = []
+    for music in music_list:
+        current_music_url = music["music_url"]
+        current_music_name = music["music_name"]
+        current_music_artist = music["music_artist"]
+        current_music_picture_url = music["music_picture_url"]
+        one_music =  {
+            'music_url': current_music_url,
+            'music_name': current_music_name,
+            'music_artist': current_music_artist,
+            'music_picture_url': current_music_picture_url,
+        }
+        to_send_music_info_list.append(one_music)
+    return to_send_music_info_list
+
+
 
 class AdminIndexHandler(AdminBaseHandler):
     @tornado.web.authenticated
@@ -10,3 +32,14 @@ class AdminIndexHandler(AdminBaseHandler):
             self.redirect(self.get_argument('next', '/adminlogin'))
             return
         self.render('adminindex.html', admin_user=self.current_user)
+
+
+    @tornado.web.authenticated
+    def post(self):
+        action = self.get_argument("action", "default")
+
+        if action == "refresh":
+            to_send_music_info_list = generate_musics_table()
+            print to_send_music_info_list
+            self.write( json_encode(to_send_music_info_list) )
+
