c0b0d6d6e6ab8d68878b85c420dd38a3f6ec6742
yxjxx <yangjing201111335@gmail.com>
2014-07-04 22:06:43 +0800

    传送music的全部信息,增加对check_music_name的post的处理

--- handlers/adminindex.py
+++ handlers/adminindex.py
@@ -18,12 +18,20 @@ def generate_musics_table(begin_num, end_num):
         current_music_name = music["music_name"]
         current_music_artist = music["music_artist"]
         current_music_picture_url = music["music_picture_url"]
+        current_music_mood = music['music_mood']
+        current_music_zone = music.get('music_zone')
+        current_music_publish_date = music['music_publish_date']
+        current_music_style = music['music_style']
         one_music =  {
             'music_id': current_music_id,
             'music_url': current_music_url,
             'music_name': current_music_name,
             'music_artist': current_music_artist,
             'music_picture_url': current_music_picture_url,
+            'music_mood': current_music_mood,
+            'music_zone': current_music_zone,
+            'music_style': current_music_style,
+            'music_publish_date': current_music_publish_date,
         }
         to_send_music_info_list.append(one_music)
     newest_to_send_music_info_list = to_send_music_info_list
@@ -43,8 +51,8 @@ class AdminIndexHandler(AdminBaseHandler):
     @tornado.web.authenticated
     def post(self):
         action = self.get_argument("action", "default")
-        begin_num = int(self.get_argument("begin_num", "default"))
-        end_num = int(self.get_argument("end_num", "default"))
+        begin_num = int(self.get_argument("begin_num", "-1"))
+        end_num = int(self.get_argument("end_num", "-1"))
 
         if action == "refresh":
             to_send_music_info_list = generate_musics_table(begin_num, end_num)
@@ -62,4 +70,16 @@ class AdminIndexHandler(AdminBaseHandler):
                 delete_music_from_db( ObjectId(to_delete_music['music_id']) )
             to_send_music_info_list = generate_musics_table(begin_num, end_num)
             self.write( json_encode(to_send_music_info_list) )
+
+        if action == "add_music":
+            # self.write("OK");
+            pass
+
+        if action == "check_music_name":
+            music_name = self.get_argument("music_name", "default")
+            print(music_name)
+            result = is_music_name_existed(music_name)
+            print(result)
+            self.write(result)
+
 
