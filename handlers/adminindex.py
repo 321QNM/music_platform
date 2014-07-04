17e71dc88414f7b9b5ea27535edf4f4aa643ff0e
yxjxx <yangjing201111335@gmail.com>
2014-07-05 01:14:58 +0800

    完成增加音乐到数据库
    不为空,传music_publish_date,弹窗请放置相关音乐文件和图片文件到对应目录应在前端完成

--- handlers/adminindex.py
+++ handlers/adminindex.py
@@ -6,6 +6,9 @@ from models.database import *
 from tornado.escape import json_encode
 from bson.objectid import ObjectId
 
+path = '../static/music/'
+image_path = '../static/images/music_pictures/'
+
 def generate_musics_table(begin_num, end_num):
     music_list = admin_get_musics_from_db(begin_num, end_num)
     to_send_music_info_list = []
@@ -67,14 +70,31 @@ class AdminIndexHandler(AdminBaseHandler):
             self.write( json_encode(to_send_music_info_list) )
 
         if action == "add_music":
-            # self.write("OK");
-            pass
+            music_name = self.get_argument("music_name", "default")
+            music_artist = self.get_argument("music_artist", "default")
+            music_style = self.get_argument("music_style", "default")
+            music_zone = self.get_argument("music_zone", "default")
+            music_mood = self.get_argument("music_mood", "default")
+            music_url = self.get_argument("music_url", "default")
+            music_picture_url = self.get_argument("music_picture_url", "default")
+            music_publish_date = self.get_argument("music_publish_date", "default")
+            admin_add_music =  {
+                'music_url': path + music_url,
+                'music_name': music_name,
+                'music_artist': music_artist,
+                'music_picture_url': image_path + music_picture_url,
+                'music_mood': music_mood,
+                'music_zone': music_zone,
+                'music_style': music_style,
+                'music_publish_date': music_publish_date,
+            }
+            insert_music_to_database(admin_add_music)
 
         if action == "check_music_name":
             music_name = self.get_argument("music_name", "default")
-            print(music_name)
+            # print(music_name)
             result = is_music_name_existed(music_name)
-            print(result)
+            # print(result)
             self.write(result)
 
 
