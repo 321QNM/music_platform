d6754ed3289020aaff6fa30500f1d4ca692f16e5
SSSStone <2268265132@qq.com>
2014-07-04 15:37:53 +0800

    完成add按钮弹出框功能 可拖动  待测

--- static/js/adminindex.js
+++ static/js/adminindex.js
@@ -40,31 +40,6 @@ $(document).ready(function(){
         musicnum = a;
     });
 
-    $('.addBt').click(function(){
-    })
-    $('.editBt').click(function(){
-    })
-    $('.delBt').click(function(){
-        if(musicnum == -1){
-            alert("请先选中要操作的歌曲");
-        }
-        else{
-            delete_post("delete", musicnum, (pagenum-1)*13,pagenum*13);
-        }
-    })
-    function delete_post(action, musicnum, begin_num, end_num){
-        $.ajax({
-            type:"POST",
-            url:"/adminindex",
-            data: "action="+action + "&musicnum="+musicnum + "&begin_num="+ begin_num +"&end_num="+end_num,
-            success:function(msg){
-                list_obj = JSON.parse(msg);
-                // alert(list_obj);
-                load_list(list_obj);
-            }
-        })
-    }
-
     // 换页效果
     $('#a1').click(function(){
         pagenum = 1;
@@ -159,4 +134,129 @@ $(document).ready(function(){
         ajax_post("page_change",(pagenum-1)*13,pagenum*13);
         updatepageNum(pagenum);
     });
+
+
+
+    $('.delBt').click(function(){
+        if(musicnum == -1){
+            alert("请先选中要操作的歌曲");
+        }
+        else{
+            delete_post("delete", musicnum, (pagenum-1)*13,pagenum*13);
+        }
+    })
+    function delete_post(action, musicnum, begin_num, end_num){
+        $.ajax({
+            type:"POST",
+            url:"/adminindex",
+            data: "action="+action + "&musicnum="+musicnum + "&begin_num="+ begin_num +"&end_num="+end_num,
+            success:function(msg){
+                list_obj = JSON.parse(msg);
+                // alert(list_obj);
+                load_list(list_obj);
+            }
+        })
+    }
+
+    $('.addBt').click(function(){
+        showDialog();
+    })
+    $('.ui-dialog-title-closebutton').click(function(){
+        hideDialog();
+    })
+
+
+    //  获取元素对象
+    function g(id){ return document.getElementById(id); }
+
+    //  自动居中 - 登录浮层 ( el = Elemenet)
+    function autoCenter( el ){
+        var bodyW = document.documentElement.clientWidth;
+        var bodyH = document.documentElement.clientHeight;
+
+        var elW = el.offsetWidth;
+        var elH = el.offsetHeight;
+
+        el.style.left = ( bodyW - elW  ) / 2  + 'px';
+        el.style.top  = ( bodyH - elH  ) / 2  + 'px';
+    }
+
+    //  自动全屏 - 遮罩
+    function fillToBody( el ){
+        el.style.width   =  document.documentElement.clientWidth  +'px';
+        el.style.height  =  document.documentElement.clientHeight +'px';
+    }
+
+    var mouseOffsetX = 0;   //  偏移
+    var mouseOffsetY = 0;
+
+    var isDraging = false;  //  是否可拖拽的标记
+
+    //  鼠标事件1 － 在标题栏上按下（要计算鼠标相对拖拽元素的左上角的坐标，并且标记元素为可拖动）
+    g('dialogTitle').addEventListener('mousedown',function(e){
+        var e = e || window.event;
+        mouseOffsetX = e.pageX - g('dialog').offsetLeft;
+        mouseOffsetY = e.pageY - g('dialog').offsetTop;
+        isDraging = true;
+    })
+    //  鼠标事件2 － 鼠标移动时（要检测，元素是否可标记为移动，如果是，则更新元素的位置，到当前鼠标的位置［ps：要减去第一步中获得的偏移］）
+    document.onmousemove = function( e ){
+        var e = e || window.event;
+
+        var mouseX = e.pageX;   // 鼠标当前的位置
+        var mouseY = e.pageY;
+
+        var moveX = 0;  //  浮层元素的新位置
+        var moveY = 0;
+
+        if( isDraging === true ){
+
+            moveX = mouseX - mouseOffsetX;
+            moveY = mouseY - mouseOffsetY;
+
+            //  范围限定   moveX > 0 并且  moveX < (页面最大宽度 - 浮层的宽度)
+            //            moveY > 0 并且  movey < (页面最大高度 - 浮层的高度)
+
+            var pageWidth  = document.documentElement.clientWidth ;
+            var pageHeight = document.documentElement.clientHeight ;
+
+            var dialogWidth  = g('dialog').offsetWidth;
+            var dialogHeight = g('dialog').offsetHeight;
+
+            var maxX = pageWidth - dialogWidth;
+            var maxY = pageHeight- dialogHeight;
+
+            moveX = Math.min( maxX , Math.max(0,moveX) );
+            moveY = Math.min( maxY , Math.max(0,moveY) );
+
+            g('dialog').style.left = moveX + 'px';
+            g('dialog').style.top  = moveY + 'px';
+        }
+
+    }
+
+
+    //  鼠标事件3 － 鼠标松开的时候（标记元素为不可拖动即可）
+    document.onmouseup = function(){
+        isDraging = false;
+    }
+
+    //  展现登录浮层
+    function showDialog(){
+        g('dialog').style.display = 'block';
+        g('mask').style.display = 'block';
+        autoCenter(g('dialog'));
+        fillToBody( g('mask') );
+    }
+
+    //  隐藏登录浮层
+    function hideDialog(){
+        g('dialog').style.display = 'none';
+        g('mask').style.display = 'none';
+    }
+
+    onresize =function(){
+        autoCenter(g('dialog'));
+        fillToBody( g('mask') );
+    }
 });
