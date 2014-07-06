a1eef0d6a9fcd55de5100cd468ecd8e2ee59552a
SSSStone <2268265132@qq.com>
2014-07-06 19:57:04 +0800

    完成个人空间

--- static/js/myzone.js
+++ static/js/myzone.js
@@ -2,6 +2,7 @@ var like_pagenum = -1;
 var hate_pagenum = -1;
 var like_musicid = -1;
 var hate_musicid = -1;
+var info_obj;
 $(document).ready(function(){
     function like_list_post(action,kind,begin_num,end_num){
         $.ajax({
@@ -49,6 +50,25 @@ $(document).ready(function(){
             $('#hate_list' + i).html('');
         }
     };
+
+    info_post("refresh","info")
+    function info_post(){
+        $.ajax({
+            type:"POST",
+            url:"/myzone",
+            data: "action="+ action+ "&kind="+ kind,
+            success:function(msg){
+                info_obj = JSON.parse(msg);
+                load_info(info_obj);
+            }
+        })
+    }
+    function load_info(info_obj){
+        $('#name').html(info_obj[0].nickname);
+        $('#ignature').html(info_obj[0].individuality_signature);
+        $('#user_image').html('<img class="user_image" src="'+ info_obj[0].individuality_signature +'">');
+    }
+
     // 设置足迹
     $('.page i:even').css("top","-40px");
     // 刷新页码
@@ -244,6 +264,129 @@ $(document).ready(function(){
                 load_hate_list(hate_list_obj);
             }
         })
+    }
+
+
+    $('.editBt').click(function(){
+            showDialog();
+            document.getElementById('nickname').value = list_obj[temp].music_name;
+            document.getElementById('individuality_signature').value = list_obj[temp].music_artist;
+            document.getElementById('myzone_picture_url').value = list_obj[temp].music_style;
+        }
+    })
+    $('.ui-dialog-title-closebutton').click(function(){
+        hideDialog();
+    })
+    $('#submit').click(function(){
+        edit_post("edit_info");
+        hideDialog();
+        return false;
+    })
+    function edit_post(action){
+        $.ajax({
+            type:"POST",
+            url:"/myzone",
+            data: "action=" + action + "&nickname="+$("#nickname").val() +  "&individuality_signature="+$("#individuality_signature").val() + "&myzone_picture_url="+$("#myzone_picture_url").val(),
+            success:function(msg){
+                info_obj = JSON.parse(msg);
+                load_info(info_obj);
+            }
+        })
+    };
+
+
+    //  获取元素对象
+    function g(id){ return document.getElementById(id); }
+
+    //  自动居中 - 登录浮层 ( el = Elemenet)
+    function autoCenter( el ){
+        var bodyW = document.documentElement.clientWidth;
+        var bodyH = document.documentElement.clientHeight;
+
+        var elW = el.offsetWidth;
+        var elH = el.offsetHeight;
+
+        el.style.left = ( bodyW - elW  ) / 2  + 'px';
+        el.style.top  = ( bodyH - elH  ) / 2  + 'px';
+    }
+
+    //  自动全屏 - 遮罩
+    function fillToBody( el ){
+        el.style.width   =  document.documentElement.clientWidth  +'px';
+        el.style.height  =  document.documentElement.clientHeight +'px';
+    }
+
+    var mouseOffsetX = 0;   //  偏移
+    var mouseOffsetY = 0;
+
+    var isDraging = false;  //  是否可拖拽的标记
+
+    //  鼠标事件1 － 在标题栏上按下（要计算鼠标相对拖拽元素的左上角的坐标，并且标记元素为可拖动）
+    g('dialogTitle').addEventListener('mousedown',function(e){
+        var e = e || window.event;
+        mouseOffsetX = e.pageX - g('dialog').offsetLeft;
+        mouseOffsetY = e.pageY - g('dialog').offsetTop;
+        isDraging = true;
+    })
+    //  鼠标事件2 － 鼠标移动时（要检测，元素是否可标记为移动，如果是，则更新元素的位置，到当前鼠标的位置［ps：要减去第一步中获得的偏移］）
+    document.onmousemove = function( e ){
+        var e = e || window.event;
+
+        var mouseX = e.pageX;   // 鼠标当前的位置
+        var mouseY = e.pageY;
+
+        var moveX = 0;  //  浮层元素的新位置
+        var moveY = 0;
+
+        if( isDraging === true ){
+
+            moveX = mouseX - mouseOffsetX;
+            moveY = mouseY - mouseOffsetY;
+
+            //  范围限定   moveX > 0 并且  moveX < (页面最大宽度 - 浮层的宽度)
+            //            moveY > 0 并且  movey < (页面最大高度 - 浮层的高度)
+
+            var pageWidth  = document.documentElement.clientWidth ;
+            var pageHeight = document.documentElement.clientHeight ;
+
+            var dialogWidth  = g('dialog').offsetWidth;
+            var dialogHeight = g('dialog').offsetHeight;
+
+            var maxX = pageWidth - dialogWidth;
+            var maxY = pageHeight- dialogHeight;
+
+            moveX = Math.min( maxX , Math.max(0,moveX) );
+            moveY = Math.min( maxY , Math.max(0,moveY) );
+
+            g('dialog').style.left = moveX + 'px';
+            g('dialog').style.top  = moveY + 'px';
+        }
+
+    }
+
+
+    //  鼠标事件3 － 鼠标松开的时候（标记元素为不可拖动即可）
+    document.onmouseup = function(){
+        isDraging = false;
+    }
+
+    //  展现登录浮层
+    function showDialog(){
+        g('dialog').style.display = 'block';
+        g('mask').style.display = 'block';
+        autoCenter(g('dialog'));
+        fillToBody( g('mask') );
+    }
+
+    //  隐藏登录浮层
+    function hideDialog(){
+        g('dialog').style.display = 'none';
+        g('mask').style.display = 'none';
+    }
+
+    onresize =function(){
+        autoCenter(g('dialog'));
+        fillToBody( g('mask') );
     }
 
 })
