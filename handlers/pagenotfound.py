98ddbc282cb0ccb414fee0eca015f945910655be
yxjxx <yangjing201111335@gmail.com>
2014-07-08 23:21:00 +0800

    random 404 page

--- handlers/pagenotfound.py
+++ handlers/pagenotfound.py
@@ -1,8 +1,9 @@
 #coding=utf-8
 
 import tornado.web
+import random
 
 class PageNotFoundHandler(tornado.web.RequestHandler):
     def write_error(self, status_code, **kwargs):
-        # self.write("Gosh darnit, user! You caused a %d erroe." % status_code)
-        self.render('404.html')
+        html_file_list = ['404_1.html', '404_2.html']
+        self.render(html_file_list[random.randint(0,1)])
