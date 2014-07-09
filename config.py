bff31b28c571f26153fd7e3aff8f6d0bba5ad339
yxjxx <yangjing201111335@gmail.com>
2014-07-09 15:43:24 +0800

    定义常量字典

--- config.py
+++ config.py
@@ -9,3 +9,13 @@ SETTINGS = dict(
     xsrf_cookie = True,
     debug = True,
 )
+
+CONSTANT = dict(
+    init_score = 100,
+    like_increase_score = 2,
+    hate_decrease_score = 2,
+    next_decrease_score = 1,
+)
+
+if __name__ == '__main__':
+    print CONSTANT['init_score']
